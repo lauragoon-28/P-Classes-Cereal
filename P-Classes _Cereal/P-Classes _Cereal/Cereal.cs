d08aaefdbcd88fded50a19f33c2cc5f25ab69245
Laura Goon <Laura.goon@ou.edu>
2021-04-22 18:08:51 -0500

    started the project and added the file and created the classes

---
+++ P-Classes _Cereal/P-Classes _Cereal/Cereal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P_Classes__Cereal
+{
+    class Cereal
+    {
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public double Calories { get; set; }
+
+        public double Cups { get; set; }
+
+        public Cereal()
+        {
+            Manufacturer = "";
+            Name = "";
+            Calories = 0;
+            Cups = 0;
+        }
+
+    }
+}
---
+++ P-Classes _Cereal/P-Classes _Cereal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace P_Classes__Cereal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //We want to read in a data set of Cereal information and help answer a few questions for the user.  
+            //The application should output all of the Cereal information that have a serving size that is 1 cup or more.  
+            //After you output all those cereals, we also need to inform them which Cereals have 100 calories or less per serving.
+
+            string[] productInfo = File.ReadAllLines("Cereal_Data.txt");
+
+            double cups = Convert.ToDouble(productInfo[3]);
+
+            if (cups >= 1)
+            {
+                Console.WriteLine(productInfo);
+            }
+
+
+
+        }
+    }
+}
