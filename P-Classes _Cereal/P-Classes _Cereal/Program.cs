9eb2467f13af2a4f1262f8eb3a2f711278f1767f
Laura Goon <Laura.goon@ou.edu>
2021-04-27 15:32:48 -0500

    finished Participation

--- P-Classes _Cereal/P-Classes _Cereal/Program.cs
+++ P-Classes _Cereal/P-Classes _Cereal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace P_Classes__Cereal
@@ -13,16 +14,41 @@ namespace P_Classes__Cereal
             //After you output all those cereals, we also need to inform them which Cereals have 100 calories or less per serving.
 
             string[] productInfo = File.ReadAllLines("Cereal_Data.txt");
+            //for (int i = 0; i < productInfo.Length; i++)
+            //{
+            //    Console.WriteLine(productInfo[i]);
+            //}
 
-            double cups = Convert.ToDouble(productInfo[3]);
+            List<Cereal> cereals = new List<Cereal>();
 
-            if (cups >= 1)
+            for (int i = 1; i < productInfo.Length; i++)
             {
-                Console.WriteLine(productInfo);
+                string[] currentProduct = productInfo[i].Split('|');
+                Cereal c1 = new Cereal();
+                c1.Name = currentProduct[0];
+                c1.Manufacturer = currentProduct[1];
+                c1.Calories = Convert.ToDouble(currentProduct[2]);
+                c1.Cups = Convert.ToDouble(currentProduct[3]);
+                cereals.Add(c1);
             }
 
+            Console.WriteLine("Cereals with a serving size with 1 cup or more:");
+            for (int i = 0; i < cereals.Count; i++)
+            {
+                if (cereals[i].Cups >= 1)
+                {
+                    Console.WriteLine($"    - Name: {cereals[i].Name}, Manufacturer: {cereals[i].Manufacturer}, Calories: {cereals[i].Calories}, Cups: {cereals[i].Cups}");
+                }
+            }
 
-
+            Console.WriteLine("Cereals with 100 calories or less per serving:");
+            for (int i = 0; i < cereals.Count; i++)
+            {
+                if (cereals[i].Calories <= 100)
+                {
+                    Console.WriteLine($"    - Name: {cereals[i].Name}, Manufacturer: {cereals[i].Manufacturer}, Calories: {cereals[i].Calories}, Cups: {cereals[i].Cups}");
+                }
+            }
         }
     }
 }
